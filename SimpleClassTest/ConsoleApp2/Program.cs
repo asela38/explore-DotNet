92a6ea60008b9268a4595deb50bf83b6b7abfe9a
INTRA\asela.illayapparachc <asela.illayapparachc@rakuten.com>
2018-11-20 19:35:40 +0800

    few simple applications

---
+++ SimpleClassTest/ConsoleApp2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Calculator
+    {
+        public int add(int i , int j)
+        {
+            return i + j;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new Calculator().add(10,11));
+            Console.ReadKey();
+
+            PersonNS.Person person = new PersonNS.Person();
+            person.firstName = "A";
+            person.lastName = "B";
+            Console.WriteLine(person.introduce());
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ SimpleClassTest/ConsoleApp2/Users/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonNS
+{
+    
+        public class Person
+        {
+            public string firstName;
+            public string lastName;
+
+            public string introduce()
+            {
+                return ("My Name is " + firstName + " lastName " + lastName);
+
+            }
+        }
+    
+}
